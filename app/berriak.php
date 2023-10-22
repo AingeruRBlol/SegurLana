a7092bf4c0cdd0bec2bb4e82d64f575142b7385b
AingeruRBlol <83420475+AingeruRBlol@users.noreply.github.com>
2023-10-23 00:37:11 +0200

    Add files via upload

---
+++ app/berriak.php
@@ -0,0 +1,70 @@
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>STONKX</title>
+	<link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+	<nav>
+        <nav class="nav">
+        <div class="nav__container">
+        <h1 class="nav__title">STONKX</h1>
+		<ul class="menu-horizontal">
+        <li><a href="index.php">Hasiera</a></li>
+            <li>
+				<a href="#">Zapatilak</a>
+				<ul class="menu-vertical">
+					<li><a href="zapatilak.php">Nireak</a></li>
+					<li><a href="zapatilak_add.php">Gehitu</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="berriak.php">Berriak</a>
+			</li>
+            <li>
+				<a href="#">Saioa</a>
+				<ul class="menu-vertical">
+					<li><a href="login.php">Saioa hasi</a></li>
+					<li><a href="signup.php">Erregistratu</a></li>
+				</ul>
+			</li>
+            <li>
+				<a href="profile.php">Profila</a>
+                
+			</li>
+</ul>
+</div>
+</nav>
+		
+	</nav>
+</body>
+<nav>
+<body>
+    <header>
+        <h1>Azken Berriak</h1>
+    </header>
+    </nav>
+</body>
+<nav>
+<body>
+    <header>
+        <h1>Nike Día de Muertos 2023</h1> 
+        <p>2023ko Urriaren 18a</p>
+    </header>
+    <p> Jadanik famatuak egin diren Día de Muertos Nike zapatilak, urtero bezala dendetan izango ditugu.
+        Aurtengoan, Nike Air Max 1 modeloa eta Air Jordan 1 Zoom modeloak egongo dira eskuragai. Beti bezala, zapatila hauek lortzeko Nike SNKRS app-a erabili beharko da, eta Urriaren 20an eta 21ean aterako dira merkatura.
+    </p>
+    <div align="center">
+    <img class="emp" src="irudi/Air-Max-1-Dia-de-Muertos.jpeg" auto; width="750" height="550">
+    <p>
+        Air Max 1 modeloa
+    </p>
+    <img class="emp" src="irudi/NIKE-COLECCION-DIA-DE-MUERTOS-2023-.jpg" width="750" height="550">
+    <p>
+        Air Jordan 1 Zoom modeloa
+    </p>
+    </div>
+</nav>
+</body>
+</html>
